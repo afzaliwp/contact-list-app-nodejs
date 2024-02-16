04e69653370195860ed22f5c787847dc993d03c4
Mohammad Afzali <afzaliwp@gmail.com>
2024-02-16 10:37:18 +0330

    add menu for the app

--- index.js
+++ index.js
@@ -4,26 +4,87 @@ import {
     stdout as output,
 } from 'process';
 
-const rl = readline.createInterface( {input, output} );
-const contactsList = [];
+const rl = readline.createInterface({input, output});
 
-const firstName = await rl.question('First Name: ');
-const lastName = await rl.question('last Name: ');
+const contactsList = [
+    {
+        id: 0,
+        firstName: 'Mamad',
+        lastName: 'afzali'
+    }
+];
 
-const contact = {
-    id: contactsList.length,
-    firstName,
-    lastName,
+const validMenuOptions = ['n', 'l', 'q']
+
+async function help() {
+    console.log('n: add new contact\nl: show list\nq: quit');
+    const menu = await rl.question('Enter an option from the menu: ');
+
+    if ( ! validMenuOptions.includes(menu) ) {
+        console.log( '--- Invalid Input! ---' );
+        quit();
+    }
+
+    if ('l' === menu) {
+        showContactsList(contactsList);
+        console.log( '---------- List Done. ----------' );
+        help();
+    }
+
+    if ('n' === menu) {
+        await addNewContact();
+        showContactsList(contactsList);
+        console.log( '---------- New Contact added! ----------' );
+        help();
+    }
+
+    if ('q' === menu) {
+        console.log('--- Bye! ---');
+        quit();
+        return;
+    }
+
 }
-contactsList.push(contact);
 
-const formattedContactList = contactsList
-    .map( ({id, firstName, lastName}) => {
-    return `#${id}   ${firstName}    ${lastName}`;
-} ).join();
+async function addNewContact() {
+    const firstName = await rl.question('First Name: ');
+    const lastName = await rl.question('last Name: ');
 
-console.log( '----------------------------' );
-console.log( formattedContactList );
-console.log( '----------------------------' );
+    const contact = {
+        id: contactsList.length,
+        firstName,
+        lastName,
+    }
+    contactsList.push(contact);
 
-rl.close();
+    return contactsList;
+}
+
+/*
+ * showType: 'table' or 'normal'
+ */
+function showContactsList(contactsList, showType = 'table') {
+    if ('table' === showType) {
+        console.table(contactsList);
+        return;
+    }
+
+    const formattedContactList = contactsList
+        .map(({id, firstName, lastName}) => {
+            return `#${id}   ${firstName}    ${lastName}`;
+        }).join();
+
+    console.log('----------------------------');
+    console.log(formattedContactList);
+    console.log('----------------------------');
+}
+
+function quit() {
+    rl.close();
+}
+
+help();
+
+// const list = await addNewContact();
+// showContactsList(list, 'table');
+// quit();
