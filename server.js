aae54a81d0114a389b0ba9806c8124c875f392ad
Mohammad Afzali <afzaliwp@gmail.com>
2024-02-19 15:33:06 +0330

    rewrite the server with express

--- server.js
+++ server.js
@@ -1,34 +1,37 @@
-import http from 'http';
-import url from 'url';
+import express from 'express';
 import {loadContacts, printTable} from "./services.js";
 
 const contactList = [];
 
-const server = http.createServer((req, res) => {
-    console.log(req.method, req.url);
-    const urlObj = url.parse(req.url, true);
-    const {format} = urlObj.query;
+const app = express();
 
-    if ( 'table' === format ) {
-        res.setHeader('Content-Type', 'text/html');
-        res.writeHead(200);
-        res.write(printTable(contactList));
-        res.end();
-    } else {
-        res.setHeader('Content-Type', 'application/json');
-        res.writeHead(200);
-        res.write(JSON.stringify(contactList));
-        res.end();
-    }
-});
+const logRequests = function (req, res, next) {
+    console.log(req.method, req.url)
+    next();
+}
+
+app.use(logRequests);
+
+const handleRoutes = () => {
+    app.get('/contacts', (req, res) => {
+        if ( 'table' === req.query.format ) {
+            res.setHeader('Content-Type', 'text/html');
+            res.status(200).send(printTable(contactList));
+        } else {
+            res.status(200).json(contactList);
+        }
+    });
+}
+
 
 async function main() {
     const contacts = await loadContacts();
     contactList.push(...contacts);
+    handleRoutes();
 
-    server.listen(4000, () => {
-        console.log('app is running on port 4000, http://localhost:4000')
-    });
+    app.listen( 4000, () => {
+        console.log('app is running on port 4000, http://localhost:4000');
+    } );
 }
 
 await main();
