f14104c9257ba3b32108a15fd8545bcc5bc0ae7a
Mohammad Afzali <afzaliwp@gmail.com>
2024-03-01 22:14:14 +0330

    add a note on postgresql how to connect

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 import pgPkg from 'pg';
 const { Pool } = pgPkg;
 
+// server running command:
+// psql -h localhost -p 54321 -U ma90 -d contacts
 const pool = new Pool({
     host: 'localhost',
     port: 54321,
